4eebf9aee6b5d9267039957ac0d55649fcb25c50
Jerry Li <jerryauckland@gmail.com>
2023-07-10 16:42:55 +1200

    feat: Init report component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const reportElement = screen.getByText(/Report/i);
+  expect(reportElement).toBeInTheDocument();
 });
---
+++ src/components/report.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export type ReportProps = {}
+
+export const Report = (props: ReportProps) => {
+  return (
+    <div>Report</div>
+  )
+}
+
+export default Report;
